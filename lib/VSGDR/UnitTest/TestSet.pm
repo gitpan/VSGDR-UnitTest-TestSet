a186754f9bb7545d01d8d1e415064e56cf895e3a
D Medved <CENSORED@hotmail.comb>
2012-10-23 10:28:05 +0000

    Import of DEDMEDVED/VSGDR-UnitTest-TestSet-1.04 from CPAN.
    gitpan-cpan-distribution: VSGDR-UnitTest-TestSet
    gitpan-cpan-version:      1.04
    gitpan-cpan-path:         DEDMEDVED/VSGDR-UnitTest-TestSet-1.04.tar.gz
    gitpan-cpan-author:       DEDMEDVED
    gitpan-cpan-maturity:     released

--- lib/VSGDR/UnitTest/TestSet.pm
+++ lib/VSGDR/UnitTest/TestSet.pm
@@ -10,11 +10,11 @@ VSGDR::UnitTest::TestSet - Sealed class for Microsoft Visual Studio Database Edi
 
 =head1 VERSION
 
-Version 1.02
+Version 1.04
 
 =cut
 
-our $VERSION = '1.03';
+our $VERSION = '1.04';
 
 
 use autodie qw(:all);
